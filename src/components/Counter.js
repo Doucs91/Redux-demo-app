eb3c4564120b0a1e7f4550f4f1efae77fa6109c9
Sady Doucoure <sadydoucoure@gmail.com>
2021-09-01 16:27:35 -0400

    Adding action types

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,21 +3,28 @@ import classes from "./Counter.module.css";
 
 const Counter = () => {
   const counter = useSelector((state) => state.counter);
+  const show = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
-  const incrementHandler= ()=>{
-    dispatch({type:'increment'});
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
   };
-  const decrementHandler= ()=>{
-    dispatch({type:'decrement'});
+  const increaseHandler = () => {
+    dispatch({ type: "increase", amount: Math.floor(Math.random() * 10) });
   };
-  const toggleCounterHandler = () => {};
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increaseHandler}>Increase</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
